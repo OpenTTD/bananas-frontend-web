e1fc7245ea0309eb3ef7129687b2b4531de5ff31
frosch <frosch@openttd.org>
2020-04-19 19:58:37 +0200

    Add: TOS 1.3 with updates to the repackaging.

--- webclient/pages/static.py
+++ webclient/pages/static.py
@@ -11,6 +11,7 @@ _tos_versions = {
     "1.0": "tos-1.0.html",
     "1.1": "tos-1.1.html",
     "1.2": "tos-1.2.html",
+    "1.3": "tos-1.3.html",
 }
 
 
@@ -28,7 +29,7 @@ def healthz_handler():
 
 @app.route("/manager/tos")
 def tos_latest():
-    return redirect("tos", version="1.2")
+    return redirect("tos", version="1.3")
 
 
 @app.route("/manager/tos/<version>")
