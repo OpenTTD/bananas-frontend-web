2f9880c39ab88372d6ae806f25ce7b6f71ca3f65
Patric Stout <truebrain@openttd.org>
2020-04-27 11:34:25 +0200

    Fix: cache the content-type overview, as it is ~0.5MB of API data
    This should improve the performance of the system as a whole.

--- webclient/pages/package_list.py
+++ webclient/pages/package_list.py
@@ -1,18 +1,33 @@
+import time
+
 from ..app import app
 from ..api import api_get
 from ..helpers import template
 from ..session import protected
 
+CACHE_EXPIRE_TIME = 60 * 5
+
+_content_type_cache_expire = {}
+_content_type_cache = {}
+
 
 @app.route("/package/<content_type>")
 def package_list(content_type):
-    packages = api_get(("package", content_type))
+    # As this API is ~0.5 MiB in response body, cache it for 5 minutes. This
+    # reduces the load of the system as a whole.
+    if _content_type_cache_expire.get(content_type, 0) > time.time():
+        packages = _content_type_cache[content_type]
+    else:
+        packages = api_get(("package", content_type))
 
-    for package in packages:
-        package["latest"] = max(package.get("versions", []), default=None, key=lambda v: v.get("upload-date", ""))
+        for package in packages:
+            package["latest"] = max(package.get("versions", []), default=None, key=lambda v: v.get("upload-date", ""))
 
-    packages.sort(key=lambda p: p.get("name", ""))
-    packages.sort(reverse=True, key=lambda p: p["latest"].get("upload-date", "") if p["latest"] else "")
+        packages.sort(key=lambda p: p.get("name", ""))
+        packages.sort(reverse=True, key=lambda p: p["latest"].get("upload-date", "") if p["latest"] else "")
+
+        _content_type_cache_expire[content_type] = time.time() + CACHE_EXPIRE_TIME
+        _content_type_cache[content_type] = packages
 
     return template("package_list.html", content_type=content_type, packages=packages)
 
