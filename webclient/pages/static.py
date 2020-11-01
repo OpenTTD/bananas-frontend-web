0e0fe314af417a1e08ed0020bc5061c1570f9e14
Patric Stout <truebrain@openttd.org>
2020-11-01 20:59:41 +0100

    Add: introduce more clear guidelines in the use of GPL licenses
    Given the following scenario:
    - Author A creates a GRF under GPL v2
    - Author B picks up that work and extends on it
    If user B wants to upload to BaNaNaS, the only way he can do that
    is by complying to GPL v2. In order to be compliant, a link to
    the source code has to be supplied. This commit makes it more
    clear to the authors that this is expected of them.
    
    This is not meant to protect OpenTTD, but more to be a good
    neighbour and ensure authors are aware of the implications of
    the license they used (to base their work on).

--- webclient/pages/static.py
+++ webclient/pages/static.py
@@ -11,6 +11,7 @@ _tos_versions = {
     "1.1": "tos-1.1.html",
     "1.2": "tos-1.2.html",
     "1.3": "tos-1.3.html",
+    "1.4": "tos-1.4.html",
 }
 
 
@@ -33,7 +34,7 @@ def user_migration():
 
 @app.route("/manager/tos")
 def tos_latest():
-    return redirect("tos", version="1.3")
+    return redirect("tos", version="1.4")
 
 
 @app.route("/manager/tos/<version>")
