0e6f00300de2b65d49b2944e9ef0d1f080ea85db
Patric Stout <truebrain@openttd.org>
2023-06-09 14:59:16 +0200

    Add: allow specifying which remote-ip header to trust

--- webclient/__main__.py
+++ webclient/__main__.py
@@ -6,8 +6,8 @@ from openttd_helpers.sentry_helper import click_sentry
 from werkzeug import serving
 
 from . import pages  # noqa
+from . import app
 from .api import click_api
-from .app import app
 from .helpers import click_urls
 from .session import click_max_age
 
@@ -23,14 +23,19 @@ original_log_request = serving.WSGIRequestHandler.log_request
 serving.WSGIRequestHandler.log_request = log_request
 
 
-@click.group(cls=flask.cli.FlaskGroup, create_app=lambda: app)
+@click.group(cls=flask.cli.FlaskGroup, create_app=lambda: app.app)
 @click_logging  # Should always be on top, as it initializes the logging
 @click_sentry
 @click_urls
 @click_api
 @click_max_age
-def cli():
-    pass
+@click.option(
+    "--remote-ip-header",
+    help="Header which contains the remote IP address. Make sure you trust this header!",
+)
+def cli(remote_ip_header):
+    if remote_ip_header:
+        app.REMOTE_IP_HEADER = f"HTTP_{remote_ip_header.upper().replace('-', '_')}"
 
 
 if __name__ == "__main__":
