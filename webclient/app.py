be2abe73e957f85103c654a948433c07e2b909da
Patric Stout <truebrain@openttd.org>
2023-06-09 15:12:06 +0200

    Add: allow specifying which remote-ip header to trust (#212)

--- webclient/app.py
+++ webclient/app.py
@@ -1,3 +1,20 @@
 import flask
 
+REMOTE_IP_HEADER = None
+
+
+class ProxyFix:
+    def __init__(self, app) -> None:
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        if REMOTE_IP_HEADER:
+            remote_ip = environ.get(REMOTE_IP_HEADER)
+            if remote_ip:
+                environ["REMOTE_ADDR"] = remote_ip
+
+        return self.app(environ, start_response)
+
+
 app = flask.Flask("webclient")
+app.wsgi_app = ProxyFix(app.wsgi_app)
